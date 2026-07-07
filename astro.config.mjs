675822d851ff983df942c70603d146052d680ec7
WONGLING <wongling@example.com>
2026-07-08 02:50:02 +0800

    Add sitemap configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import react from '@astrojs/react';
 import keystatic from '@keystatic/astro';
 import vercel from '@astrojs/vercel';
 import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://astro-personal-site.vercel.app', // ⚠️ 請在此處更換成您正確的線上網址，以便產生精準的 sitemap.xml
   adapter: vercel(),
-  integrations: [react(), mdx(), keystatic()]
+  integrations: [react(), mdx(), keystatic(), sitemap()]
 });
